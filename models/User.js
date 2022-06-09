4c5317d603dd341944bd01cf543205cae2e3d98b
JinSeok <seok8440@naver.com>
2022-06-09 19:43:31 +0900

    로그아웃 기능 추가

--- models/User.js
+++ models/User.js
@@ -59,7 +59,6 @@ userSchema.methods.comparePassword = function (plainPassword, cb) {
 
 userSchema.methods.generateToken = function (cb) {
     var user = this;
-    // console.log('user._id', user._id)
 
     // jsonwebtoken을 이용해서 token을 생성하기 
     var token = jwt.sign(user._id.toHexString(), 'secretToken')
@@ -71,6 +70,20 @@ userSchema.methods.generateToken = function (cb) {
     user.save(function (err, user) {
         if (err) return cb(err)
         cb(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+    // user._id + ''  = token
+    //토큰을 decode 한다. 
+    jwt.verify(token, 'secretToken', function (err, decoded) {
+        //유저 아이디를 이용해서 유저를 찾은 다음에 
+        //클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+        user.findOne({ "_id": decoded, "token": token }, function (err, user) {
+            if (err) return cb(err);
+            cb(null, user)
+        })
     })
 }
 

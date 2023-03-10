2d0d1ae4480c983f2fdbb6ebea9f0b7f424fff2b
JinSeok <seok8440@naver.com>
2023-03-10 15:57:25 +0900

    cors 이슈,proxy 설정, concurrently, antd

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios';
 
 function LandingPage() {
+
+  //LandingPage에 들어오자마자 실행
+  useEffect(() => {
+    axios.get('/api/hello')
+    .then(response => console.log(response.data))
+  }, [])
+
   return (
     <div>LandingPage</div>
   )
--- server/index.js
+++ server/index.js
@@ -24,6 +24,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/api/hello', (req, res) => {
+  res.send("hi")
+})
+
 app.post('/api/users/register', (req, res) => {
   //회원가입할 때 필요한 정보들을 client에서 가져오면
   //그것들을 데이터베이스에 넣어준다.

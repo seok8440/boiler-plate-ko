f9edbc43abaf82b132fe83b52d72865e00212625
JinSeok <seok8440@naver.com>
2023-03-13 23:49:06 +0900

    오류찾기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,10 @@
 import React, { useEffect } from 'react'
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function LandingPage() {
+
+  const navigate = useNavigate();
 
   //LandingPage에 들어오자마자 실행
   useEffect(() => {
@@ -9,9 +12,31 @@ function LandingPage() {
     .then(response => console.log(response.data))
   }, [])
 
-  return (
-    <div>LandingPage</div>
-  )
+  const onClickHandler = () => {
+    axios.get(`/api/users/logout`)
+        .then(response => {
+            if (response.data.success) {
+              navigate('/login');
+            } else {
+                alert('로그아웃 하는데 실패 했습니다.')
+            }
+        })
+}
+
+return (
+    <div style={{
+        display: 'flex', justifyContent: 'center', alignItems: 'center'
+        , width: '100%', height: '100vh'
+    }}>
+        <h2>시작 페이지</h2>
+
+        <button onClick={onClickHandler}>
+            로그아웃
+        </button>
+
+    </div>
+)
+
 }
 
 export default LandingPage
--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import { applyMiddleware, legacy_createStore } from 'redux';
-import promiseMiddleware from 'redux-promise';
-import ReduxThunk from 'redux-thunk';
-import Reducer from './_reducers';
+// import { applyMiddleware, legacy_createStore } from 'redux';
+// import promiseMiddleware from 'redux-promise';
+// import ReduxThunk from 'redux-thunk';
 
-const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk) (legacy_createStore)
+// const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk) (legacy_createStore)
+const root = createRoot(document.getElementById("root"));
 
-ReactDOM.render(
-  <Provider
-      store={createStoreWithMiddleware(Reducer,
-          window.__REDUX_DEVTOOLS_EXTENSION__ &&
-          window.__REDUX_DEVTOOLS_EXTENSION__()
-      )}
-  >
-      <App />
-  </Provider>
-  , document.getElementById('root'));
+// ReactDOM.render(
+//   <Provider
+//       store={createStoreWithMiddleware(Reducer,
+//           window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//           window.__REDUX_DEVTOOLS_EXTENSION__()
+//       )}
+//   >
+//       <App />
+//   </Provider>
+//   , document.getElementById('root'));
+setInterval(() => {
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  }, 1);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
